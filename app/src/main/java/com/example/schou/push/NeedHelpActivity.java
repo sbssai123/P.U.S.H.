7c9decde1a34fccd81e532a1a4ad6f1d20cdbfa6
Tina Lee <l.tn3314@gmail.com>
2017-02-12 08:37:18 -0500

    texting with notifs

--- app/src/main/java/com/example/schou/push/NeedHelpActivity.java
+++ app/src/main/java/com/example/schou/push/NeedHelpActivity.java
@@ -4,13 +4,15 @@ package com.example.schou.push;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class NeedHelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BsyncTask bysncTask = new BsyncTask();
-        bysncTask.execute();
+        //setContentView(R.layout.need_help);
+        new BsyncTask().execute();
+        Log.d("MADE IT", "HERE");
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
     }
