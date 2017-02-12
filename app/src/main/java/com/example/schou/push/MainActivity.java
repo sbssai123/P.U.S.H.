2b5c7de5b277d1a62ab2bfd1981751f9bc4b1535
sarahchou <chou.s@husky.neu.edu>
2017-02-11 23:55:07 -0500

    Updated AppData and added more functionality to Activity pages.

--- app/src/main/java/com/example/schou/push/MainActivity.java
+++ app/src/main/java/com/example/schou/push/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.schou.push;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Main page with instructions for new users.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/schou/push/NewGroupActivity.java
+++ app/src/main/java/com/example/schou/push/NewGroupActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 
 public class NewGroupActivity extends AppCompatActivity implements View.OnClickListener{
     Button addGroupButton;
+    Button back;
     ListView listOfMembers;
 
     @Override
@@ -17,9 +18,13 @@ public class NewGroupActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_new_group);
 
         addGroupButton = (Button)findViewById(R.id.addMemberButton);
+        back = (Button)findViewById(R.id.backToGroupsButton);
+
         listOfMembers = (ListView)findViewById(R.id.listOfMembers);
 
         addGroupButton.setOnClickListener(this);
+        back = (Button)findViewById(R.id.backToGroupsButton);
+
         //todo change this!!
         AppData.members[0].put("joe", 123);
         //update what is in the hashmap at 0 and change it to uppercase to notify users of the current group chosen
@@ -32,6 +37,11 @@ public class NewGroupActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         if (v.getId() == R.id.addMemberButton) {
             Intent intent = new Intent(this, MemberActivity.class);
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.backToGroupsButton) {
+            Intent intent = new Intent(this, GroupActivity.class);
             startActivity(intent);
         }
     }
